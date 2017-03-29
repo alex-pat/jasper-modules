7d0f6243e1bf1e731cb6c3dc963849f771d80eb8
Alexander Pateenok <pateenoc@gmail.com>
2017-03-29 09:50:30 +0300

    WolphramAlpha plugin

---
+++ wolfram.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8-*-
+import random
+import re
+import wolframalpha
+import time
+import sys
+from sys import maxint
+
+
+WORDS = ["WHO", "WHAT", "HOW MUCH", "HOW MANY", "HOW OLD"]
+
+
+def handle(self, text, mic):
+    app_id = self.profile['keys']['WOLFRAMALPHA']
+    client = wolframalpha.Client(app_id)
+
+    query = client.query(text)
+    if len(query.pods) > 0:
+        texts = ""
+        pod = query.pods[1]
+        if pod.text:
+            texts = pod.text
+        else:
+            texts = "I can not find anything"
+
+        mic.say(texts.replace("|", ""))
+    else:
+        mic.say("Sorry, Could you be more specific?.")
+
+
+def is_valid(self, text):
+    return any(p.lower() in text.lower() for p in self.get_phrases())
